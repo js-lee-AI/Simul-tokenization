80a7117356fe7ee4f24067137868869d8166e7c9
JungSeob Lee <60927808+js-lee-AI@users.noreply.github.com>
2022-02-22 19:19:54 +0900

    Update train_tokenizers.py

--- train_tokenizers.py
+++ train_tokenizers.py
@@ -7,91 +7,104 @@ import sentencepiece as spm
 from omegaconf.listconfig import ListConfig
 from omegaconf.dictconfig import DictConfig
 
-class Spm_trainer:    
+
+class SPMTrainer:
     def __init__(
         self,
-        cfg_path = './configs/',
-        cfg_name = 'vocab_baseline.yaml',
-    ):  
-        self.cfg_path_and_name = cfg_path + cfg_name
+        cfg_path="./config",
+        cfg_name="vocab_baseline.yaml",
+    ):
+        self.cfg_path_and_name = os.path.join(cfg_path, cfg_name)
         hydra.initialize(cfg_path, cfg_name)
-        
+
         self.configs = hydra.compose(cfg_name)
-        
         self.multiple_vocabsize = False
-        
-        # same using tokenizer methods
+
+        # encoder/decoder uses same tokenization methods
         if isinstance(self.configs.path.corpus_name, str):
-            self.path_corpus = self.configs.path.data_dir + self.configs.path.corpus_name
+            self.path_corpus = os.path.join(
+                self.configs.path.data_dir, self.configs.path.corpus_name
+            )
             self.split_srctgt = False
+        # encoder/decoder uses different tokenization methods
         elif isinstance(self.configs.path.corpus_name, DictConfig):
-            self.path_corpus = {"src": self.configs.path.data_dir + self.configs.path.corpus_name["src"],
-                                "tgt": self.configs.path.data_dir + self.configs.path.corpus_name["tgt"],
+            self.path_corpus = {
+                "src": os.path.join(
+                    self.configs.path.data_dir, self.configs.path.corpus_name["src"]
+                ),
+                "tgt": os.path.join(
+                    self.configs.path.data_dir, self.configs.path.corpus_name["tgt"]
+                ),
             }
             self.split_srctgt = True
         else:
-            raise NotImplementedError
-       
-        self.prints_args()
+            raise ValueError(
+                f"'{self.configs.path.corpus_name}' must be either string or dictionary, \not {type(self.configs.path.corpus_name)}"
+            )
+
+        self.print_args()
         self.check_configs()
-         
+        self.move_to_output_path()
+
         self.prefix = self.set_tokenizer_prefix(
-            multiple_flag = self.multiple_vocabsize,
-            vocab_sizes = self.configs.tokenizer.vocab_size,
-            cfg_name = cfg_name,
-            corpus_name = self.configs.path.corpus_name,
-            vocab_type = self.configs.tokenizer.vocab_type,
-            vocab_languages = self.configs.tokenizer.vocab_languages,
+            multiple_flag=self.multiple_vocabsize,
+            vocab_sizes=self.configs.tokenizer.vocab_size,
+            cfg_name=cfg_name,
+            corpus_name=self.configs.path.corpus_name,
+            vocab_type=self.configs.tokenizer.vocab_type,
+            vocab_languages=self.configs.tokenizer.vocab_languages,
         )
-     
-    def prints_args(self):
-        self.check_and_move_path()
-        print(f'Your config file: {self.cfg_path_and_name}')
-        print(f'Your configs: ')
-        print(self.configs)
-        #time.sleep(7)
-        
+
+    def print_args(self):
+        print(f"Your config file: {self.cfg_path_and_name}")
+        print(f"Your configs: ")
+        time.sleep(7)
+
     def check_configs(self):
-        print(self.configs.tokenizer.vocab_size, type(self.configs.tokenizer.vocab_size))
+        available_vocab_types = ["unigram", "bpe", "word", "char"]
+
         if isinstance(self.configs.tokenizer.vocab_size, ListConfig):
             self.multiple_vocabsize = True
-        
+
         if isinstance(self.configs.tokenizer.vocab_type, str):
-            if not self.configs.tokenizer.vocab_type in ['unigram', 'bpe', 'word', 'char']:
-                raise AssertionError(f'{self.configs.tokenizer.vocab_type} is not supported.')          
+            if self.configs.tokenizer.vocab_type not in available_vocab_types:
+                raise ValueError(
+                    f"'{self.configs.tokenizer.vocab_type}' is not in {available_vocab_types}."
+                )
         elif isinstance(self.configs.tokenizer.vocab_type, DictConfig):
             for vocab_type in self.configs.tokenizer.vocab_type.values():
-                if not vocab_type in ['unigram', 'bpe', 'word', 'char']:
-                    raise AssertionError(f'{self.configs.tokenizer.vocab_type} is not supported.') 
-        
-    def check_and_move_path(self):
-        if not os.path.exists(self.configs.path.data_dir):
-            raise OSError('{} directory is not exists.'.format(self.configs.path.data_dir))
-        
-        if self.split_srctgt == True:
+                if vocab_type not in available_vocab_types:
+                    raise ValueError(
+                        f"'{self.configs.tokenizer.vocab_type}' is not in {available_vocab_types}."
+                    )
+
+        if not os.path.isdir(self.configs.path.data_dir):
+            raise OSError(
+                "'{}' directory is not exist.".format(self.configs.path.data_dir)
+            )
+
+        if self.split_srctgt is True:
             for idx, (key, path) in enumerate(self.path_corpus.items()):
-                if not os.path.exists(path):
-                    raise OSError('{} directory is not exists.'.format(path))
+                if not os.path.isfile(path):
+                    raise OSError("'{}' file is not exist.".format(path))
                 else:
                     print(f'Vocab Training Courpus Files; {idx+1}) {key}: "{path}"')
                 # get abstract paths
                 self.path_corpus[key] = os.path.abspath(self.path_corpus[key])
-                
         else:
-            if not os.path.exists(self.path_corpus):
-                raise FileNotFoundError('"{}" file is not exists.'.format(self.path_corpus))
+            if not os.path.isfile(self.path_corpus):
+                raise FileNotFoundError(
+                    '"{}" file is not exist.'.format(self.path_corpus)
+                )
             else:
                 print(f'Vocab Training Courpus File: "{self.path_corpus}"')
-            
-            # get abstract path
+            # get abstract paths
             self.path_corpus = os.path.abspath(self.path_corpus)
-        
-        # make directory then move output path 
-        if not os.path.exists(os.path.abspath(self.configs.path.output_path)):
-            os.mkdir(os.path.abspath(self.configs.path.output_path))
-        else:
-            os.chdir(os.path.abspath(self.configs.path.output_path))
-    
+
+    def move_to_output_path(self):
+        os.makedirs(self.configs.path.output_path, exist_ok=True)
+        os.chdir(os.path.abspath(self.configs.path.output_path))
+
     @staticmethod
     def set_tokenizer_prefix(
         multiple_flag,
@@ -101,79 +114,113 @@ class Spm_trainer:
         vocab_type,
         vocab_languages,
     ):
-        # split tokenization (not same tokenization methods)
+        # encoder/decoder use not same tokenization method
         if isinstance(corpus_name, DictConfig):
             prefix = dict()
-            prefix['src'] = [vocab_languages['src'] + '_' + vocab_type['src'] + '_' + str(vocab_size[0]//1000) + 'k' 
-                            for vocab_size in vocab_sizes]
-            prefix['tgt'] = [vocab_languages['tgt'] + '_' + vocab_type['tgt'] + '_' + str(vocab_size[1]//1000) + 'k' 
-                            for vocab_size in vocab_sizes]
+            prefix["src"] = [
+                vocab_languages["src"]
+                + "_"
+                + vocab_type["src"]
+                + "_"
+                + str(vocab_size[0] // 1000)
+                + "k"
+                for vocab_size in vocab_sizes
+            ]
+            prefix["tgt"] = [
+                vocab_languages["tgt"]
+                + "_"
+                + vocab_type["tgt"]
+                + "_"
+                + str(vocab_size[1] // 1000)
+                + "k"
+                for vocab_size in vocab_sizes
+            ]
             return prefix
-            
-        # same using tokenization method
+
+        # encoder/decoder use same tokenization method
         elif isinstance(corpus_name, str):
             # multiple vocab size
-            if multiple_flag == True:
-                return [cfg_name[:-5] + '_' + str(vocab_size//1000) + 'k' 
-                            for vocab_size in vocab_sizes]
+            if multiple_flag is True:
+                return [
+                    vocab_languages
+                    + "_"
+                    + cfg_name[:-5]
+                    + "_"
+                    + str(vocab_size // 1000)
+                    + "k"
+                    for vocab_size in vocab_sizes
+                ]
             # single vocab size
             else:
-                return cfg_name[:-5] + '_' + str(self.configs.tokenizer.vocab_size//1000) + 'k' 
+                return (
+                    vocab_languages
+                    + "_"
+                    + cfg_name[:-5]
+                    + "_"
+                    + str(vocab_sizes // 1000)
+                    + "k"
+                )
         else:
             raise NotImplementedError
 
-        
     def train(self):
-        print('\n')
+        print("Train SPM...")
         # training split tokenizers
-        if self.split_srctgt == True:
-            print('Train SPM...')
+        if self.split_srctgt is True:
             for idx, (key, prefixes) in enumerate(self.prefix.items()):
-                keys = ['src', 'tgt']
-                for prefix, vocab_size in zip(prefixes, self.configs.tokenizer.vocab_size):
-                    spm.SentencePieceTrainer.Train(f'--input={self.path_corpus[keys[idx]]} \
+                keys = ["src", "tgt"]
+                for prefix, vocab_size in zip(
+                    prefixes, self.configs.tokenizer.vocab_size
+                ):
+                    spm.SentencePieceTrainer.Train(
+                        f"--input={self.path_corpus[keys[idx]]} \
                                                      --model_prefix={prefix} \
                                                      --vocab_size={vocab_size[idx]} \
                                                      --model_type={self.configs.tokenizer.vocab_type[keys[idx]]} \
                                                      --num_threads={self.configs.tokenizer.args.num_threads} \
-                                                     --max_sentence_length=99999')
-                    print(f'Training {prefix} Tokenizer succeed')
-            print('Training All of Tokenizers Completely succeed')
-                
+                                                     --max_sentence_length={self.configs.tokenizer.max_sentence_length}"
+                    )
+                    print(f"Training {prefix} tokenizer succeed")
         else:
             # training multiple tokenizers
-            if self.multiple_vocabsize == True:
-                print('Train SPM...')
+            if self.multiple_vocabsize is True:
                 for idx, prefix in enumerate(self.prefix):
-                    spm.SentencePieceTrainer.Train(f'--input={self.path_corpus} \
+                    spm.SentencePieceTrainer.Train(
+                        f"--input={self.path_corpus} \
                                                      --model_prefix={prefix} \
                                                      --vocab_size={self.configs.tokenizer.vocab_size[idx]} \
                                                      --model_type={self.configs.tokenizer.vocab_type} \
                                                      --num_threads={self.configs.tokenizer.args.num_threads} \
-                                                     --max_sentence_length=99999')
-                    print(f'Training {self.prefix} Tokenizer succeed')
-                print('Training All of Tokenizers Completely succeed')
+                                                     --max_sentence_length={self.configs.tokenizer.max_sentence_length}"
+                    )
+                    print(f"Training {self.prefix} tokenizer succeed")
             # training single tokenizer
             else:
-                print('Train SPM...')
-                spm.SentencePieceTrainer.Train(f'--input={self.path_corpus} \
-                                                 --model_prefix={self.configs.tokenizer.prefix} \
+                spm.SentencePieceTrainer.Train(
+                    f"--input={self.path_corpus} \
+                                                 --model_prefix={self.prefix} \
                                                  --vocab_size={self.configs.tokenizer.vocab_size} \
                                                  --model_type={self.configs.tokenizer.vocab_type} \
                                                  --num_threads={self.configs.tokenizer.args.num_threads} \
-                                                 --max_sentence_length=99999')
-                print('Training Tokenizer Completely succeed')
-                print('Finish...')
-            
+                                                 --max_sentence_length={self.configs.tokenizer.max_sentence_length}"
+                )
+                print("Training tokenizer completely succeed")
+        print("Finish...")
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Set the Hydra Configuration file')
-    parser.add_argument('--cfg-name', '-c', type=str,
-                        default='vocab_baseline.yaml',
-                        help='hydra config file name (.yalm)')
+    parser = argparse.ArgumentParser(description="Hydra configuration file")
+    parser.add_argument(
+        "--cfg-name",
+        "-c",
+        type=str,
+        default="vocab_baseline.yaml",
+        help="hydra config file name (.yalm)",
+    )
     args = parser.parse_args()
-    
-    trainer = Spm_trainer(
-        cfg_path = './configs/', 
-        cfg_name = args.cfg_name,
+
+    trainer = SPMTrainer(
+        cfg_path="./config",
+        cfg_name=args.cfg_name,
     )
     trainer.train()
