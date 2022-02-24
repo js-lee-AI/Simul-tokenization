721f019d88661f8203dd0debd314e5fccd0d5da0
JungSeob Lee <60927808+js-lee-AI@users.noreply.github.com>
2022-02-25 04:13:37 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'simultokenizer',
+    version          = '0.0',
+    description      = '',
+    author           = 'Jungseob Lee',
+    author_email     = 'omanma1928@naver.com',
+    url              = 'https://github.com/js-lee-AI/Simul-tokenization',
+    install_requires = [],
+    packages         = find_packages(exclude = []),
+    keywords         = ['liquibase', 'db migration'],
+    python_requires  = '>=3',
+    zip_safe         = False,
+    classifiers      = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ]
+)
